656726858d2075e5f0e1c00f6ad6eed1330ca9f7
AdamK <adamkoziel@outlook.com>
2020-05-12 15:30:04 +0100

    three functions added

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -8,6 +8,18 @@ def add(first_number, second_number)
   return first_number + second_number
 end
 
+def subtract(x,y)
+  return x-y
+end
+
+def multiply(x,y)
+  return x*y
+end
+
+def divide(x,y)
+  return x/y
+end
+
 def return_10()
     return 10
 end
--- specs/ruby_functions_spec.rb
+++ specs/ruby_functions_spec.rb
@@ -30,14 +30,14 @@ class FunctionsTest < MiniTest::Test
     subtract_result = subtract( 10, 5 )
     assert_equal( 5, subtract_result )
   end
-#   def test_multiply()
-#     multiply_result = multiply( 4, 2 )
-#     assert_equal( 8, multiply_result )
-#   end
-#   def test_divide()
-#     divide_result = divide( 10, 2 )
-#     assert_equal( 5, divide_result )
-#   end
+  def test_multiply()
+    multiply_result = multiply( 4, 2 )
+    assert_equal( 8, multiply_result )
+  end
+  def test_divide()
+    divide_result = divide( 10, 2 )
+    assert_equal( 5, divide_result )
+  end
 #   def test_length_of_string()
 #     test_string = "A string of length 21"
 #     length_of_string = length_of_string( test_string )
